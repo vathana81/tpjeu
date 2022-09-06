cea24fc0451974b4176eeb3f3f55a30113a24db6
vathana <vathana.chiv@hotmail.com>
2022-09-06 14:58:11 +0200

    creation personnage

--- creation.py
+++ creation.py
@@ -1,26 +1,7 @@
 #!/usr/bin/env python
 #-- coding:utf-8 --
 
-import random
-
 def createPerso(pseudo,pv,force,armure):
-    arrayPerso=""
-    arrayPerso[pseudo,pv,force,armure]
+    arrayPerso=[pseudo,pv,force,armure]
     return arrayPerso
-
-def degatMonstre(pseudo):
-    pv=random.randint(5,20)
-    force=random.randint(3,8)
-    armure=random.randint(0,5)
-    arrayDegat=[pseudo,pv,force,armure]
-    return arrayDegat
-a=degatMonstre("martin")
-print(a)
-   
-
-def degatPerso(array,degats):
-    pvRestant=""
-    pvRestant=array[1]-degats
-    return pvRestant
-
     
