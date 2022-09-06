7080946a4bb09f01fc1b3bb1af2f21d97b7ff14a
vathana <vathana.chiv@hotmail.com>
2022-09-06 14:18:21 +0200

    creation personnage

--- creation.py
+++ creation.py
@@ -1,19 +1,24 @@
 #!/Usr/bin/env python
 #-- coding:utf-8 --
 
-arrayPerso=""
-arrayDegat=""
-pvRestant=""
-
 def createPerso(pseudo,pv,force,armure):
+    arrayPerso=""
     arrayPerso[pseudo,pv,force,armure]
     return arrayPerso
 
-def degat(pseudo,pv,force,armure):
-    arrayDegat[pseudo,pv,force,armure]
+def degatMonstre(pseudo):
+    pv=random.randrange(5,20)
+    force=random.randrange(3,8)
+    armure=random.randrange(0,5)
+	arrayDegat=[pseudo,pv,force,armure]
+    return arrayDegat
+
+print(f"{mon_tuple[0]} + {mon_tuple[1]} = {mon_tuple[2]}")
     return arrayDegat
 
 def degatPerso(array,degats):
+    pvRestant=""
     pvRestant=array[1]-degats
     return pvRestant
 
+    
