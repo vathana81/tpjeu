65a543040beec8206f20932de1697191a9d8e7db
vathana <vathana.chiv@hotmail.com>
2022-09-06 13:18:50 +0200

    creation perso et generation armure

---
+++ TPGit/creation.py
@@ -0,0 +1,34 @@
+#!/Usr/bin/env python
+#-- coding:utf-8 --
+
+
+
+def createPerso(pseudo,pv,force,armure):
+    arrayPerso[pseudo,pv,force,armure]
+    return arrayPerso
+
+def degat(pseudo,pv,force,armure):
+    arraydegat[pseudo,pv,force,armure]
+    return arraydegat
+
+def degatperso(array,degats):
+    pvrestant=array[1]-degats
+    return pvrestant
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
