06695149932357c940ce94a634bc5722c856414c
vathana81 <112860457+vathana81@users.noreply.github.com>
2022-09-06 16:48:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Sep  6 15:47:37 2022
-
 @author: Administrateur
 """
 
-import creationPerso, generationMonstre
-from compteur import *
+import creationPerso.py, generationMonstre.py, cptr.py, createMonster.py, degats.py, gestioncombat.py
 
 compteurKills= 0
 playAgain = 0
+persoMort = ""
 
-while playagain == 0:
-    nomPerso = input("choisissez un perso : ")
-    monPerso = personnage.personnage(nomPerso,20,6,3)
-    while monPerso[1] > 0:
-        Ennemi = creationMonstre.creationMonstre()
-        fight.fight(monPerso, Ennemi)
+while playAgain == 0:
+    statPerso = creationPerso.createPerso("Louis",20,50,15)
+    
+    while statPerso[1] > 0:
+        monstre = createMonster.createMonster()
+        gestioncombat.gestioncombat(statPerso, monstre)
         
-    if monPerso[1] > 0:
-        compteurKills=compteurEnnemiisTrue(compteurKills)
+    if statPerso[1] > 0:
+        compteurKills=cptr.compteur(compteurKills)
+    if statPerso[1] <= 0:
+        while persoMort!="O" or persoMort!="N":
+            persoMort=input("Continuer 'O' ou 'N'")
+    if persoMort=="O":
+        print("Continuez à jouer !")
+        playAgain = 0
+    elif persoMort=="N":
+        print("Game Over !")
+        playAgain = 1
