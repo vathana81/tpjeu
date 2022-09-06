df52d9dd4cb5403badfebffc2dda60eed9779cd0
yacime <yacime@orange.fr>
2022-09-06 14:33:42 +0200

    modif calcul degats 2

--- degats.py
+++ degats.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-pv_att=7
-armure_def=2
-def attaque():
-    coup=pv_att-armure_def
-    print(coup)
-attaque()
+
+#force_att=7
+#armure_def=2
+#def attaque():
+#    coup=force_att-armure_def
+#    print(coup)
+#attaque()
+
+def gestDegats(pvdef, forceatt, armuredef):
+    if (forceatt-armuredef):
+        return pvdef-(forceatt-armuredef)
+    else:
+        return pvdef-1
